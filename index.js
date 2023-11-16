fcfe0064414e71dbe06341f57383917a00cde7c1
Jonntz <monteirojonatas23@gmail.com>
2023-11-16 19:12:20 -0300

    Add a cron job to run the script every day at 18:55

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const puppeteer = require('puppeteer');
-const readlineSync = require('readline-sync');
+const run = require('./puppeteer');
+const cron = require('node-cron');
 
-const run = async () => {
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
+const task = cron.schedule('55 18 * * *', () => {
+    console.log('running a task every minute');
+    run();
+}, {
+    timezone: "America/Sao_Paulo"
+});
 
-    const initial = readlineSync.question('Informe uma moeda inicial: ') || 'dolar';
-    const final = readlineSync.question('Informe uma moeda final: ') || 'dolar';
-
-    // Navigate the page to a URL
-    await page.goto(`https://www.google.com/search?q=${initial}+para+${final}&rlz=1C1CHBD_pt-PTBR1063BR1064&oq=dolar+para&gs_lcrp=EgZjaHJvbWUqDQgAEAAYgwEYsQMYgAQyDQgAEAAYgwEYsQMYgAQyBggBEEUYOTINCAIQABiDARixAxiABDIHCAMQABiABDINCAQQABiDARixAxiABDIKCAUQABixAxiABDIHCAYQABiABDINCAcQABiDARixAxiABDIHCAgQABiABDIHCAkQABiABKgCALACAA&sourceid=chrome&ie=UTF-8`);
-
-    const value = await page.evaluate(() => {
-        return document.querySelector('.lWzCpb.a61j6').value
-    });
-
-    console.log(`O resultado de 1 ${initial} em ${final} é ${value}`);
-    await browser.close();
-}
-
-run()
+task.start()
---
+++ puppeteer.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
+
+const run = async () => {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    const initial = readlineSync.question('Informe uma moeda inicial: ') || 'dolar';
+    const final = readlineSync.question('Informe uma moeda final: ') || 'dolar';
+
+    // Navigate the page to a URL
+    await page.goto(`https://www.google.com/search?q=${initial}+para+${final}&rlz=1C1CHBD_pt-PTBR1063BR1064&oq=dolar+para&gs_lcrp=EgZjaHJvbWUqDQgAEAAYgwEYsQMYgAQyDQgAEAAYgwEYsQMYgAQyBggBEEUYOTINCAIQABiDARixAxiABDIHCAMQABiABDINCAQQABiDARixAxiABDIKCAUQABixAxiABDIHCAYQABiABDINCAcQABiDARixAxiABDIHCAgQABiABDIHCAkQABiABKgCALACAA&sourceid=chrome&ie=UTF-8`);
+
+    const value = await page.evaluate(() => {
+        return document.querySelector('.lWzCpb.a61j6').value
+    });
+
+    console.log(`O resultado de 1 ${initial} em ${final} é ${value}`);
+    await browser.close();
+};
+
+module.exports = run
